a8ac8d0bda4b271ddfb9e8ca4d86fcf7d99ac742
MattE <49365433+mneckel@users.noreply.github.com>
2024-11-21 09:56:15 -0700

    Create verserip.py

---
+++ verserip.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+'''
+VERSE RIPPER
+---------------------------
+This will extract all the verses from a txt document into a nice list.
+Verse formatting is assumed to be from BSF study questions.  
+ref:  https://www.bsfinternational.org/
+
+1. Copy-paste the study questions into a text file.
+2. Execute at command line against the file like this:  "verserip questions.txt"
+3. Copy-paste the list of verses into your verse retrieval application. 
+You can then use this list for retrieval from your favorite application.
+Like this one:  https://www.blueletterbible.org/search.cfm#srchMulti
+
+'''
+
+import argparse
+import re
+
+print('running')
+parser = argparse.ArgumentParser(description='Process a file.')
+parser.add_argument('filename', type=str, help='The name of the file to process')
+args = parser.parse_args()
+with open(args.filename, 'r') as f:
+    # Process the file here
+    orgtxt = f.read()
+# a list of tuples is returned
+wholeverse = r"((\d*)\s+([A-Z]\w*\b)\s+(\d+[:]\d+[-]?\d?\d?\d?))|((?:((\d*)\s+)?(?:[A-Z]\w*\b)\s+)?(\d+[:]\d+[-]?\d?\d?\d?))"
+matches = re.findall(wholeverse, orgtxt)
+# print(matches)
+print("--------------------------------------------------")
+
+# Account for subsequent verses in the same book
+for book in matches:
+    if book[2] != '':     # Yes book is specified
+        prebook = book[1]+" "+book[2]
+        lookup = (book[1]+" "+book[2]+" "+book[3])
+    if book[2] == '':     # assume use of the same book specified previously
+        lookup = (prebook+" "+book[4])
+    print(lookup.lstrip())
+
